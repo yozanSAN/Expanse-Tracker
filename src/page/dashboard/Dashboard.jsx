e9ced714a482206f1d2e4f30c29274a63cc5a4a4
yozanSAN <ayoubpro183@gmail.com>
2025-12-20 18:45:46 +0100

    starting RECENT TRANSACTIONS UI

--- src/page/dashboard/Dashboard.jsx
+++ src/page/dashboard/Dashboard.jsx
@@ -3,17 +3,18 @@ import increasingLogo from '../../assets/increasing.png';
 import incomeLogo from '../../assets/incomeLogo.png';
 import decreasingLogo from '../../assets/decreasingLogo.png';
 import expensesLogo from '../../assets/expensesLogo.png';
+// import coffeStarBucksIcon from '../../assets/coffeStarbucksIcon.png'
 
 //DUMMY DATA IMPORTS
 // import { categories } from './../../data/categories';
-// import { transactions } from './../../data/transactions';
+import { transactions } from './../../data/transactions';
 import { stats } from '../../data/stats';
 
 
 export default function Dashboard() {
   return (
     <div className='bg-primary h-screen'>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-full ml-44 my-5 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-full ml-44 my-5 ">
 
         <div className='flex flex-col justify-between items-center rounded-lg border-none px-6 py-5 gap-4 my-5 bg-secondary'>
           <div className='flex justify-start items-center gap-2'>
@@ -51,8 +52,6 @@ export default function Dashboard() {
             <p className='text-textSecondary text-xl '>vs last month</p>
           </div>
         </div>
-
-
 
         <div className='flex flex-col justify-between items-center rounded-lg border-none px-6 py-5 gap-4 bg-secondary my-5'>
           <div className='flex justify-start items-center gap-2'>
@@ -142,6 +141,58 @@ export default function Dashboard() {
         </div>
 
       </div>
+
+      <div className='text-textPrimary flex justify-around items-center'>
+        <div>
+          <p className='text-xl font-bold'>Recent Transactions</p>
+        </div>
+        <div className='flex justify-between items-center gap-4'>
+          <select className='bg-secondary px-3 py-2 rounded-xl'>
+            <option value="This Month">This Month</option>
+            <option value="This Week">This Week</option>
+            <option value="This Year">This Year</option>
+          </select>
+          <select className='bg-secondary  px-3 py-2 rounded-xl'>
+            <option value="All categories">All categories</option>
+            <option value="Food & Dinning">Food & Dinning</option>
+            <option value="Shopping">Shopping</option>
+            <option value="Transport">Transport</option>
+            <option value="Others">Others</option>
+          </select>
+        </div>
+      </div>
+      {/* RECENT TRANSACTIONS GRID*/}
+
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  gap-7 max-w-full ml-44 my-5'>
+
+        {
+          Array.isArray(transactions) && transactions.length > 0 ? (
+            transactions.map(transaction =>
+              <div className='flex justify-evenly items-center bg-secondary text-textPrimary rounded-lg border-none px-6 py-5 gap-4 my-2 '>
+
+                <div >
+                  <img src={transaction.icon} className='rounded-md max-w-7' />
+                </div>
+                <div >
+                  <p className='text-md  font-bold mb-3'>{transaction.title}</p>
+                  <p className='text-textSecondary'>{transaction.category}</p>
+                </div>
+                <div>
+                  <p className='text-xl font-bold mb-3'>{transaction.amount}</p>
+                  <p className='text-textMuted'>{transaction.date}</p>
+                </div>
+              </div>
+            )
+
+          )
+            :
+            (
+              <h1>No transactions found!!</h1>
+            )
+        }
+      </div>
+
     </div>
   )
 };
