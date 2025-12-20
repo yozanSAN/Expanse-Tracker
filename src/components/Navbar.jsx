201b9ac57672dcbcd35b4c397005aa19b42b47f8
yozanSAN <ayoubpro183@gmail.com>
2025-12-20 15:59:08 +0100

    updating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 import expenseTrackerLogo from '../assets/expenses.png';
-
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../config/firebase-config';
+import userDefaultPhoto from '../assets/default-avatar.png'
 export default function Navbar() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <nav className='bg-gradient-to-t from-secondary to-secondary flex justify-around items-center py-4 gap-20 '>
-        <div>
-            <p className='font-bold text-2xl text-textPrimary'>ExpenseTracker</p>
-        </div>
-        <div>
-            <img src={expenseTrackerLogo} className='w-10 h-19 rounded bg-accent' />
-        </div>
+    <nav className='bg-gradient-to-t from-secondary to-secondary  py-4 gap-20 '>
+      {
+        user ? (
+          <div className='flex justify-around items-center  gap-20 '>
+            <section className='flex justify-between items-center gap-3'>
+              <img src={expenseTrackerLogo} className='w-10 h-19 rounded bg-accent' />
+              <p className='font-bold text-2xl text-textPrimary'>ExpenseTracker</p>
+            </section>
+            <img src={user.photoURL || userDefaultPhoto} className='w-10 h-19 rounded-full' />
+          </div>
+        )
+          :
+          (
+            <div className='flex justify-around items-center'>
+              <p className='font-bold text-2xl text-textPrimary'>ExpenseTracker</p>
+              <img src={expenseTrackerLogo} className='w-10 h-19 rounded bg-accent' />
+            </div>
+          )
+      }
     </nav>
   )
 };
